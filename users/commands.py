f253533a976553c2677d7492e952a5a67c3ad78b
ssweezy <aslanokaraf@gmail.com>
2023-12-10 22:20:11 +0300

    error fixes

--- users/commands.py
+++ users/commands.py
@@ -9,9 +9,9 @@ async def func_start(message: Message):
     if not db.user_exists(message.from_user.id):
         db.add_user(message.from_user.id, message.from_user.username, message.from_user.first_name,
                     message.from_user.last_name, message.from_user.url)
-    await message.answer('bobo')
+        
     kb = [
-        [KeyboardButton(text="открыть swzy!", web_app=WebAppInfo(url="https://lambent-faun-dcab07.netlify.app/"))]
+        [KeyboardButton(text="открыть swzy!", web_app=WebAppInfo(url=""))]
     ]
     keyboard = ReplyKeyboardMarkup(
         keyboard=kb,
