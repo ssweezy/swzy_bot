59cb0d9f2cd93f1aa1b8ebc11be1788bd301dfd2
ssweezy <aslanokaraf@gmail.com>
2023-12-12 23:59:52 +0300

    verdion.sultan

--- users/commands.py
+++ users/commands.py
@@ -1,4 +1,5 @@
-from aiogram.types import Message, WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, WebAppInfo, InlineKeyboardButton, ReplyKeyboardRemove
+from aiogram.utils.keyboard import InlineKeyboardBuilder  
 from users.db import DataBase
 
 db = DataBase('swzy_db')
@@ -10,11 +11,10 @@ async def func_start(message: Message):
         db.add_user(message.from_user.id, message.from_user.username, message.from_user.first_name,
                     message.from_user.last_name, message.from_user.url)
         
-    kb = [
-        [KeyboardButton(text="открыть swzy!", web_app=WebAppInfo(url="https://iridescent-duckanoo-20b130.netlify.app/"))]
-    ]
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True)
-    
-    await message.answer(f"welcome to SWZY! co ltd", reply_markup=keyboard)
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(
+        text="открыть swzy!",
+        web_app=WebAppInfo(url="https://iridescent-duckanoo-20b130.netlify.app/"))
+    )
+    await message.answer("bo", reply_markup=ReplyKeyboardRemove())
+    await message.answer(f"welcome to SWZY! co ltd", reply_markup=builder.as_markup())
